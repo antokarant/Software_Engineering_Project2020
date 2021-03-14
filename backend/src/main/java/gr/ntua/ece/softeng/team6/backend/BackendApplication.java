f2e9908b4b5b01a1335526f4e085c272af8e62ac
antokarant <antonis@karantonis.net>
2021-03-14 12:31:39 +0200

    Minor changes

--- backend/src/main/java/gr/ntua/ece/softeng/team6/backend/BackendApplication.java
+++ backend/src/main/java/gr/ntua/ece/softeng/team6/backend/BackendApplication.java
@@ -31,6 +31,7 @@ public class BackendApplication {
 				.antMatchers(HttpMethod.POST, "/login").permitAll()
 				.antMatchers(HttpMethod.POST, "/admin/usermod/**").hasRole("ADMIN")
 				.antMatchers(HttpMethod.POST, "/admin/resetsessions").permitAll()
+				.antMatchers(HttpMethod.GET, "/admin/healthcheck").permitAll()
 				.antMatchers(HttpMethod.GET, "/admin/**").hasRole("ADMIN")
 				.antMatchers(HttpMethod.GET, "/users/**").permitAll()
 				.antMatchers(HttpMethod.POST, "/admin/system/sessionsupd").hasRole("ADMIN")
